faace3887e6da2f919da909c141e0a7cbc6c7d7c
kiel <joelrykiel@live.com>
2021-09-17 15:33:43 +0800

    changed itemcontroller to item(s)controller

---
+++ ASPNetCoreMasters/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMasters.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet("{userId}")]
+        public IEnumerable<String> GetAll(int userId)
+        {
+            var item = new ItemService();
+            var result = item.GetAll(userId);
+
+            return result;
+        }
+
+    }
+}
