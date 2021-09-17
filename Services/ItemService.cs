486b1c215326af72351e23659e48d87f8a414cf0
kiel <joelrykiel@live.com>
2021-09-17 15:26:17 +0800

    initial commit

---
+++ Services/ItemService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public class ItemService
+    {
+        public IEnumerable<string> GetAll(int userId)
+        {
+            var arr = new string[] {"Book", "Pencil", "Pen" };
+            return arr;
+        }
+
+    }
+}
