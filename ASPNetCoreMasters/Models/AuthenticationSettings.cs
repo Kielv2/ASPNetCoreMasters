0e28ca8bf107a41255a8fd176232cc5440054251
kiel <joelrykiel@live.com>
2021-10-01 15:47:04 +0800

    Added Sesson6 hw

---
+++ ASPNetCoreMasters/Models/AuthenticationSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreMasters.Models
+{
+    public class AuthenticationSettings
+    {
+        public JWT JWT { get; set; }
+    }
+
+    public class JWT
+    {
+        public string SecurityKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
